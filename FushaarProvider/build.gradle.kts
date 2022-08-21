580bfce94fb7e1d2913441f12231d5865b20ed80
Lazycoder <aymenozomaki@gmail.com>
2022-08-21 04:14:38 +0200

    Add files via upload

---
+++ FushaarProvider/build.gradle.kts
@@ -0,0 +1,12 @@
+version = 1
+
+cloudstream {
+    description = ""
+    authors = listOf( "ImZaw" )
+
+    status = 1
+
+    tvTypes = listOf( "Movie")
+
+    iconUrl = "https://www.google.com/s2/favicons?domain=www.fushaar.com&sz=%size%"
+}
