ef14c7de6ef888a44fb061166b589a254919e3d8
Zaw <42999156+ImZaw@users.noreply.github.com>
2024-04-09 04:16:45 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,7 +13,7 @@ buildscript {
         classpath("com.android.tools.build:gradle:7.3.0")
         // Cloudstream gradle plugin which makes everything work and builds plugins
         classpath("com.github.recloudstream:gradle:master-SNAPSHOT")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.21")
     }
 }
 
