3a77e448dd467ae8e1fb2ce131b112f5958fae0b
Zaw <42999156+ImZaw@users.noreply.github.com>
2024-04-10 22:18:21 +0300

    Update faselhd url

--- FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
+++ FaselHDProvider/src/main/kotlin/com/faselhd/FaselHDProvider.kt
@@ -12,7 +12,7 @@ import org.jsoup.nodes.Element
 
 class FaselHD : MainAPI() {
     override var lang = "ar"
-    override var mainUrl = "https://faselhd.io"
+    override var mainUrl = "https://faselhd.club"
     private  val alternativeUrl = "https://www.faselhd.club"
     override var name = "FaselHD"
     override val usesWebView = false
